71206e9dd113c457be70de523e0aec096cf89491
waterlala <stu313023j@gmail.com>
2019-03-07 23:23:07 +0800

    no message

--- test/test/test.cpp
+++ test/test/test.cpp
@@ -13,7 +13,8 @@ bool Guess(int number) {
 		target = -1;
 		return true;
 	}
-	else std::cout << "Wrong" << std::endl;
+	else if (number > target) std::cout << "Smaller" << std::endl;
+	else if (number < target) std::cout << "Bigger" << std::endl;
 	return false;
 }
 int main(array<System::String ^> ^args)
