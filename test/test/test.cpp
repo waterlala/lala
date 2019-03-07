bffe87e438d1499fd0a5569c71ce9bcc6033c7d4
icelala <limyijun123@gmail.com>
2019-03-07 22:21:25 +0800

    test

---
+++ test/test/test.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main(array<System::String ^> ^args)
+{
+	cout << "helloworld" << endl;
+	system("pause");
+	return 0;
+}
